26948740d9af910d9c41657be49014878953108d
Jaylen Wang <1318248167@qq.com>
2025-12-28 23:29:29 +0800

    2025-12-28 23:29:29

---
+++ internal/infra/db/db.go
@@ -0,0 +1,65 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"openresume/internal/infra/config"
+	"openresume/internal/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitMySQL(cfg config.Config) (*gorm.DB, error) {
+	var err error
+	if cfg.MySQLDSN != "" {
+		DB, err = gorm.Open(mysql.Open(cfg.MySQLDSN), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+	} else {
+		DB, err = gorm.Open(sqlite.Open(cfg.SQLitePath), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+	}
+	if err != nil {
+		return nil, err
+	}
+	sqlDB, err := DB.DB()
+	if err != nil {
+		return nil, err
+	}
+	// prefer versioned migrations if present; otherwise fall back to AutoMigrate for initial schema
+	if _, e := os.Stat("db/migrations"); e == nil {
+		if err = RunMigrations(cfg, sqlDB); err != nil {
+			return nil, err
+		}
+	} else {
+		if err = autoMigrate(); err != nil {
+			return nil, err
+		}
+	}
+	log.Println("mysql connected")
+	return DB, nil
+}
+
+func autoMigrate() error {
+	if err := DB.AutoMigrate(
+		&models.User{},
+		&models.Template{},
+		&models.Resume{},
+		&models.ResumeSection{},
+		&models.ResumeItem{},
+		&models.ShareLink{},
+		&models.AuditLog{},
+		&models.OAuthAccount{},
+		&models.Config{},
+	); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/infra/db/migrate.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+
+	"openresume/internal/infra/config"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	"github.com/golang-migrate/migrate/v4/database/sqlite"
+)
+
+func RunMigrations(cfg config.Config, sqlDB *sql.DB) error {
+	migrationsDir := filepath.Join("db", "migrations")
+	if _, err := os.Stat(migrationsDir); err != nil {
+		return nil
+	}
+	var (
+		m   *migrate.Migrate
+		err error
+	)
+	if cfg.SQLitePath != "" {
+		driver, e := sqlite.WithInstance(sqlDB, &sqlite.Config{})
+		if e != nil {
+			return e
+		}
+		m, err = migrate.NewWithDatabaseInstance("file://"+migrationsDir, "sqlite3", driver)
+	} else {
+		driver, e := mysql.WithInstance(sqlDB, &mysql.Config{})
+		if e != nil {
+			return e
+		}
+		m, err = migrate.NewWithDatabaseInstance("file://"+migrationsDir, "mysql", driver)
+	}
+	if err != nil {
+		return err
+	}
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
